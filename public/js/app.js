6084173a716ed58d33f03852cbd8b353fcb05795
kotita49 <kotita49@gmail.com>
2021-02-05 11:27:19 +0000

    Button changing colour

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Add your JavaScript here.
+changeBackgroundColour();
+   
 });
+function changeBackgroundColour(){
+   let itemPost = document.getElementsByClassName('item-post');
+  for(let element of itemPost){
+    element.style.background = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+
+  }
+  
+   
+  
+      console.log('working test')
+      //;
+        
+  }
